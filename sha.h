a5dc381e2d79b5eed896d5c47bab65948c674a10
Andrew Clark <lark047@gmail.com>
2013-03-12 21:05:08 +0000

    reversed

--- sha.h
+++ sha.h
@@ -49,12 +49,12 @@
 
 #include <stdint.h>
 
-uint8_t *SHAstring(const char *);
-
 #if defined SHA1 || defined SHA224 || defined SHA256
 # define BLOCK_SIZE_BITS      64
 # define PADDED_LENGTH_BITS  448
 # define DIGEST_LENGTH_BITS  512
 #endif
+
+uint8_t *SHAstring(const char *);
 
 #endif /* SHA_H_ */
