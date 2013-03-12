afa3d638d6fcebe1fd1e033b8ba3a65932d56283
Andrew Clark <lark047@gmail.com>
2013-03-12 18:10:28 +0000

    derived SHA224 from SHA256

--- main-sha224.c
+++ main-sha224.c
@@ -13,52 +13,49 @@ int main(int argc, char **argv)
     {
         char * const string = argv[1];
 
-        PRINT("Calculating SHA1 for \"%s\"...\n", string);
+        PRINT("Calculating SHA224 for \"%s\"...\n", string);
         PRINT("Using byte size of %u\n", (unsigned) CHAR_BIT);
 
         uint8_t *digest = SHAstring(string);
-
-    /* 0:  */
-    /*     */
 
     /**
      * Results
      *
      * ""
-     *   expected: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-     *     actual: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+     *   expected: d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f
+     *     actual: d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f
      *
      * "a"
-     *   expected: ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
-     *     actual: ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
+     *   expected: abd37534c7d9a2efb9465de931cd7055ffdb8879563ae98078d6d6d5
+     *     actual: abd37534c7d9a2efb9465de931cd7055ffdb8879563ae98078d6d6d5
      *
      * "abc"
-     *   expected: ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-     *     actual: ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
+     *   expected: 23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7
+     *     actual: 23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7
      *
      * "message digest"
-     *   expected: f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650
-     *     actual: f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650
+     *   expected: 2cb21c83ae2f004de7e81c3c7019cbcb65b71ab656b22d6d0c39b8eb
+     *     actual: 2cb21c83ae2f004de7e81c3c7019cbcb65b71ab656b22d6d0c39b8eb
      *
      * "abcdefghijklmnopqrstuvwxyz"
-     *   expected: 71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73
-     *     actual: 71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73
+     *   expected: 45a5f72c39c5cff2522eb3429799e49e5f44b356ef926bcf390dccc2
+     *     actual: 45a5f72c39c5cff2522eb3429799e49e5f44b356ef926bcf390dccc2
      *
      * "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-     *   expected: db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0
-     *     actual: db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0
+     *   expected: bff72b4fcb7d75e5632900ac5f90d219e05e97a7bde72e740db393d9
+     *     actual: bff72b4fcb7d75e5632900ac5f90d219e05e97a7bde72e740db393d9
      *
      * "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
-     *   expected: f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e
-     *     actual: f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e
+     *   expected: b50aecbe4e9bb0b57bc5f3ae760a8e01db24f203fb3cdcd13148046e
+     *     actual: b50aecbe4e9bb0b57bc5f3ae760a8e01db24f203fb3cdcd13148046e
      *
      * "The quick brown fox jumps over the lazy dog"
-     *   expected: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
-     *     actual: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
+     *   expected: 730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525
+     *     actual: 730e109bd7a8a32b1cb9d9a09aa2325d2430587ddbc0c38bad911525
      *
      * "The quick brown fox jumps over the lazy dog."
-     *   expected: ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c
-     *     actual: ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c
+     *   expected: 619cba8e8e05826e9b8c519c0a5c68f4fb653e8a3d8aa04bb2c8cd4c
+     *     actual: 619cba8e8e05826e9b8c519c0a5c68f4fb653e8a3d8aa04bb2c8cd4c
      */
 
         printf("%s\n", digest);
