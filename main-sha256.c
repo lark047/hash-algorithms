d48de88967aa89d86c436b3c30f9e3a23e2ef1ce
Andrew Clark <lark047@gmail.com>
2013-03-12 20:53:02 +0000

    made use of hash_info and added CUnit to main-sha256.c

--- main-sha256.c
+++ main-sha256.c
@@ -3,7 +3,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <limits.h>
+
+#include <CUnit/Basic.h>
+
+static void testSHA256(void)
+{
+    const char *msgs[] = {
+        "",
+        "a",
+        "abc",
+        "message digest",
+        "abcdefghijklmnopqrstuvwxyz",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+        "The quick brown fox jumps over the lazy dog",
+        "The quick brown fox jumps over the lazy dog.",
+        0
+    };
+
+    const char *sha256s[] = {
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+        "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+        "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+        "f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650",
+        "71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73",
+        "db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0",
+        "f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e",
+        "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
+        "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c"
+    };
+    const char *actual, *expected;
+
+    for (uint8_t i = 0; msgs[i]; ++i)
+    {
+        expected = sha256s[i];
+        actual = (const char *) SHAstring(msgs[i]);
+
+        CU_ASSERT_STRING_EQUAL(actual, expected);
+
+        if (strcmp(expected, actual))
+        {
+            fprintf(stderr, "\n");
+            fprintf(stderr, "%s\n", msgs[i]);
+            fprintf(stderr, "expected: %s\n", expected);
+            fprintf(stderr, "actual  : %s\n", actual);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -11,65 +59,58 @@ int main(int argc, char **argv)
 
     if (argc == 2)
     {
-        char * const string = argv[1];
+        if (strcmp(argv[1], "-t") == 0)
+        {
+            CU_pSuite suite;
 
-        PRINT("Calculating SHA256 for \"%s\"...\n", string);
-        PRINT("Using byte size of %u\n", (unsigned) CHAR_BIT);
+            if (CU_initialize_registry() != CUE_SUCCESS)
+            {
+                rc = CU_get_error();
+            }
+            else
+            {
+                if ((suite = CU_add_suite("SHA56 Test Suite", NULL, NULL)) == NULL)
+                {
+                    CU_cleanup_registry();
+                    rc = CU_get_error();
+                }
+                else
+                {
+                    if (CU_ADD_TEST(suite, testSHA256) == NULL)
+                    {
+                        CU_cleanup_registry();
+                        rc = CU_get_error();
+                    }
+                    else
+                    {
+                        CU_basic_set_mode(CU_BRM_VERBOSE);
+                        CU_basic_run_tests();
+                        CU_cleanup_registry();
 
-        uint8_t *digest = SHAstring(string);
+                        rc = CU_get_error();
+                    }
+                }
+            }
+        }
+        else
+        {
+            char * const string = argv[1];
 
-    /* 0:  */
-    /*     */
+            PRINT("Calculating SHA256 for \"%s\"...\n", string);
+            PRINT("Using byte size of %u\n", (unsigned) CHAR_BIT);
 
-    /**
-     * Results
-     *
-     * ""
-     *   expected: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-     *     actual: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-     *
-     * "a"
-     *   expected: ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
-     *     actual: ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb
-     *
-     * "abc"
-     *   expected: ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-     *     actual: ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad
-     *
-     * "message digest"
-     *   expected: f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650
-     *     actual: f7846f55cf23e14eebeab5b4e1550cad5b509e3348fbc4efa3a1413d393cb650
-     *
-     * "abcdefghijklmnopqrstuvwxyz"
-     *   expected: 71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73
-     *     actual: 71c480df93d6ae2f1efad1447c66c9525e316218cf51fc8d9ed832f2daf18b73
-     *
-     * "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-     *   expected: db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0
-     *     actual: db4bfcbd4da0cd85a60c3c37d3fbd8805c77f15fc6b1fdfe614ee0a7c8fdb4c0
-     *
-     * "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
-     *   expected: f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e
-     *     actual: f371bc4a311f2b009eef952dd83ca80e2b60026c8e935592d0f9c308453c813e
-     *
-     * "The quick brown fox jumps over the lazy dog"
-     *   expected: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
-     *     actual: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
-     *
-     * "The quick brown fox jumps over the lazy dog."
-     *   expected: ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c
-     *     actual: ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c
-     */
+            uint8_t *digest = SHAstring(string);
 
-        printf("%s\n", digest);
+            printf("%s\n", digest);
 
-        /* clean up */
-        PRINT("%s\n", "Cleaning up...");
-        PRINT("0x%p\n", (void *) digest);
-        free(digest);
-        digest = NULL;
+            /* clean up */
+            PRINT("%s\n", "Cleaning up...");
+            PRINT("0x%p\n", (void *) digest);
+            free(digest);
+            digest = NULL;
 
-        rc = EXIT_SUCCESS;
+            rc = EXIT_SUCCESS;
+        }
     }
     else
     {
