c8d2eb3ec815391d8035c1fe3ef54cab0d40fca8
Andrew Clark <lark047@gmail.com>
2013-03-13 14:18:53 +0000

    removed obsolete hash function

--- sha224.c
+++ sha224.c
@@ -213,16 +213,3 @@ void process(uint8_t **digest, const uint32_t block_count, const uint16_t block_
 
     snprintf((char *) *digest, SHA224_LENGTH, "%08x%08x%08x%08x%08x%08x%08x", h0, h1, h2, h3, h4, h5, h6);
 }
-
-uint32_t h(uint8_t i, uint32_t x, uint32_t y, uint32_t z)
-{
-    switch ((uint8_t) (i / 20))
-    {
-        case 0: /*  0 <= i < 20 */ return (x & y) ^ (~x & z);
-        case 1: /* 20 <= i < 40 */ return x ^ y ^ z;
-        case 2: /* 40 <= i < 60 */ return (x & y) ^ (x & z) ^ (y & z);
-        case 3: /* 60 <= i < 80 */ return x ^ y ^ z;
-    }
-
-    return 0;
-}
