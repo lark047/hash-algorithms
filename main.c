29310a960e1e8a53348c54a612d51e5b48c748a6
Andrew Clark <lark047@gmail.com>
2013-03-14 15:37:22 +0000

    corrected SHA1string function name and added all hashes output

--- main.c
+++ main.c
@@ -7,12 +7,13 @@
 #include <CUnit/Basic.h>
 
 extern char *MD5string(const char *);
-extern char *SHA1String(const char *);
+extern char *SHA1string(const char *);
 extern char *SHA224string(const char *);
 extern char *SHA256string(const char *);
 extern char *SHA384string(const char *);
 extern char *SHA512string(const char *);
-/* ... */
+extern char *SHA512224string(const char *);
+/* extern char *SHA512256string(const char *); */
 
 extern void testMD5(void);
 extern void testSHA1(void);
@@ -20,7 +21,8 @@ extern void testSHA224(void);
 extern void testSHA256(void);
 extern void testSHA384(void);
 extern void testSHA512(void);
-/* ... */
+/* extern void testSHA512224(void); */
+/* extern void testSHA512256(void); */
 
 int main(int argc, char **argv)
 {
@@ -49,7 +51,9 @@ int main(int argc, char **argv)
                     CU_ADD_TEST(suite, testSHA224) == NULL ||
                     CU_ADD_TEST(suite, testSHA256) == NULL ||
                     CU_ADD_TEST(suite, testSHA384) == NULL ||
-                    CU_ADD_TEST(suite, testSHA512) == NULL)
+                    CU_ADD_TEST(suite, testSHA512) == NULL /* ||
+                    CU_ADD_TEST(suite, testSHA512224) == NULL /* ||
+                    CU_ADD_TEST(suite, testSHA512256) == NULL */)
                 {
                     CU_cleanup_registry();
                     rc = CU_get_error();
@@ -67,7 +71,18 @@ int main(int argc, char **argv)
     }
     else if (argc == 2)
     {
-        /* TODO */
+        const char *msg = argv[1];
+
+        /* printf("Hashing \"%s\"\n", msg); */
+
+        printf("       MD5: %s\n", MD5string(msg));
+        printf("      SHA1: %s\n", SHA1string(msg));
+        printf("    SHA224: %s\n", SHA224string(msg));
+        printf("    SHA256: %s\n", SHA256string(msg));
+        printf("    SHA384: %s\n", SHA384string(msg));
+        printf("    SHA512: %s\n", SHA512string(msg));
+        printf("SHA512/224: %s\n", SHA512224string(msg));
+        /* printf("SHA512/256: %s\n", SHA512256string(msg)); */
     }
 
     PRINT("Exiting with status %d\n", rc);
