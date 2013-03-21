e2ce60ffbb966a98a520c556ba429f182f282916
Andrew Clark <lark047@gmail.com>
2013-03-21 20:35:01 +0000

    added STRINGIFY, CHARS_PER_BYTE, DIGEST_LENGTH macros changed PRINT to output the file and line number

--- util.h
+++ util.h
@@ -11,19 +11,29 @@ struct hash_info
     uint16_t  digest_length;
 };
 
-uint32_t append_padding(uint8_t **, const char *, uint32_t *, struct hash_info *);
+uint64_t append_padding(uint8_t **, uint8_t *, uint64_t *, struct hash_info *);
 
 # define ROTL(v,s) (((v) << (s)) | ((v) >> (sizeof(v) * CHAR_BIT - (s))))
 # define ROTR(v,s) (((v) >> (s)) | ((v) << (sizeof(v) * CHAR_BIT - (s))))
 # define SHR(v,s)  ((v) >> (s))
 
+#define STRINGIFY_(x)  #x
+#define STRINGIFY(x)   STRINGIFY_(x)
+
 #ifdef DEBUG
-# define PRINT(f, ...)  printf("[debug] " f, __VA_ARGS__)
-void print_d(const uint8_t *, uint32_t, const struct hash_info *);
+# define PRINT(f, ...)  printf("[debug] " __FILE__ ":" STRINGIFY(__LINE__) "  " f, __VA_ARGS__)
+void print_d(const uint8_t *, uint64_t, const struct hash_info *);
 #else
 # define PRINT(f, ...)  /* NO-OP */
 #endif
 
-#define SIZE(a) (sizeof (a) / sizeof *(a))
+#define SIZE(a)     (sizeof (a) / sizeof *(a))
+#define STR_EQ(a,b)    (strcmp((a), (b)) == 0)
+
+#ifndef BIT_STRENGTH
+# define BIT_STRENGTH    0
+#endif
+#define CHARS_PER_BYTE   2
+#define DIGEST_LENGTH   (CHARS_PER_BYTE * BIT_STRENGTH / CHAR_BIT + 1)
 
 #endif /* UTIL_H_ */
