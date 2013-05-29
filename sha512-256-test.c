71e6484e78aa22d3153db7c30b9b30a68ad442a3
Andrew Clark <lark047@gmail.com>
2013-05-29 20:22:49 +0000

    fixed and verified SHA-512/256. still need to find a generator.

--- sha512-256-test.c
+++ sha512-256-test.c
@@ -1,6 +1,7 @@
 #include "sha.h"
 #include "util.h"
 
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,15 +11,25 @@
 
 #if 0
 static void testSHA512256file(void);
-static void testSHA512256string(void);
 #endif
+
+static void testSHA512256string(void);
+
+/**
+ * From: http://csrc.nist.gov/groups/ST/toolkit/documents/Examples/SHA512_256.pdf
+ */
+static char *test_msgs512256[] = {
+    "abc",
+    "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+    0
+};
 
 void testSHA512256(void)
 {
 #if 0
     testSHA512256file();
-    testSHA512256string();
 #endif
+    testSHA512256string();
 }
 
 #if 0
@@ -58,13 +69,14 @@ void testSHA512256file(void)
         fclose(fp);
     }
 }
+#endif
 
-void testSHA512256string(void)
+static void testSHA512256string(void)
 {
-    /* TODO i can't find a generator for SHA-512/256 */
+    /* TODO The examples are from the official test vectors. */
     char *sha512256s[] = {
-        "",
-        "",
+        "53048e2681941ef99b2e29b76b4c7dabe4c2d0c634fc6d46e0e2f13107e7af23",
+        "3928e184fb8690f840da3988121d31be65cb9d3ef83ee6146feac861e19b563a",
         "",
         "",
         "",
@@ -74,10 +86,14 @@ void testSHA512256string(void)
         ""
     };
 
-    for (uint8_t i = 0; test_msgs[i]; ++i)
+    for (uint8_t i = 0; test_msgs512256[i]; ++i)
     {
+        uint8_t *digest = SHA512256string(test_msgs512256[i]);
+
+        CU_ASSERT_PTR_NOT_NULL_FATAL(digest);
+
         char *expected = sha512256s[i];
-        char *actual = (char *) SHA512256string(test_msgs[i]);
+        char *actual = to_string(digest, DIGEST_LENGTH);
 
         CU_ASSERT_PTR_NOT_NULL_FATAL(actual);
         CU_ASSERT_STRING_EQUAL(actual, expected);
@@ -89,13 +105,12 @@ void testSHA512256string(void)
         else
         {
             fprintf(stderr, "\n");
-            fprintf(stderr, "string  : -->%s<--\n", test_msgs[i]);
+            fprintf(stderr, "string  : -->%s<--\n", test_msgs512256[i]);
             fprintf(stderr, "expected: %s\n", expected);
             fprintf(stderr, "actual  : %s\n", actual);
         }
 
+        free(digest);
         free(actual);
     }
 }
-
-#endif
