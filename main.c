aa990221ab6d53fa62450ba44a6c33b521f5ab04
Andrew Clark <lark047@gmail.com>
2013-08-02 16:45:55 +0000

    simplified main and fixed SHA-224 and SHA-512/224

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "sha.h"
 #include "util.h"
 
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,14 +19,18 @@ extern void testSHA224(void);
 extern void testSHA256(void);
 extern void testSHA384(void);
 extern void testSHA512(void);
-
-#if 0
 extern void testSHA512224(void);
 extern void testSHA512256(void);
-#endif
 
 static short index_of(const char *, const char *[]);
-static FILE *open(const char *, const char *);
+
+static int do_tests(const char *, const char **, ...);
+static int do_hash_strings(const char *, const char **, uint8_t *(**)(const char *), const uint8_t *);
+static int do_hash_files(const char *, const char **, uint8_t *(**)(FILE *), const uint8_t *);
+static int do_verifies(const char *, const char **, uint8_t *(**)(FILE *), const uint8_t *);
+static int do_hmacs(const char *, const char *, const char **, uint8_t *(**)(const char *, const char *), const uint8_t *);
+
+static void usage_main(const char *);
 
 int main(int argc, char **argv)
 {
@@ -45,7 +51,7 @@ int main(int argc, char **argv)
     };
 
     const uint8_t digest_lengths[] = {
-        16, 16, 16, 20, 24, 32, 48, 64, 24, 32
+        16, 16, 16, 20, 28, 32, 48, 64, 28, 32
     };
 
     uint8_t *(*hash_strings[])(const char *) = {
@@ -57,8 +63,8 @@ int main(int argc, char **argv)
         SHA256string,
         SHA384string,
         SHA512string,
-        /* SHA512224string */ NULL,
-        /* SHA512256string */ NULL
+        SHA512224string,
+        SHA512256string
     };
 
     uint8_t *(*hash_files[])(FILE *) = {
@@ -74,131 +80,64 @@ int main(int argc, char **argv)
         /* SHA512256file */ NULL
     };
 
-    if (argc == 2 && STR_EQ(argv[1], "-t"))
+    uint8_t *(*hash_hmacs[])(const char *, const char *) = {
+        MD2hmac,
+        MD4hmac,
+        MD5hmac,
+        SHA1hmac,
+        SHA224hmac,
+        SHA256hmac,
+        SHA384hmac,
+        SHA512hmac,
+        /* SHA512224hmac */ NULL,
+        /* SHA512256hmac */ NULL
+    };
+
+    switch (get_option(argc, argv))
     {
-        CU_pSuite suite;
-
-        if (CU_initialize_registry() == CUE_SUCCESS)
+        case Test:
         {
-            if ((suite = CU_add_suite("Cryptography Test Suite", NULL, NULL)))
-            {
-                if (CU_ADD_TEST(suite, testMD2)
-                 && CU_ADD_TEST(suite, testMD4)
-                 && CU_ADD_TEST(suite, testMD5)
-                 && CU_ADD_TEST(suite, testSHA1)
-                 && CU_ADD_TEST(suite, testSHA224)
-                 && CU_ADD_TEST(suite, testSHA256)
-                 && CU_ADD_TEST(suite, testSHA384)
-                 && CU_ADD_TEST(suite, testSHA512)
-#if 0
-                 && CU_ADD_TEST(suite, testSHA512224)
-                 && CU_ADD_TEST(suite, testSHA512256)
-#endif
-                 )
-                {
-                    CU_basic_set_mode(CU_BRM_VERBOSE);
-                    CU_basic_run_tests();
-                }
-            }
-            CU_cleanup_registry();
+            const char *labels[] = {
+                STRINGIFY(testMD2),
+                STRINGIFY(testMD4),
+                STRINGIFY(testMD5),
+                STRINGIFY(testSHA1),
+                STRINGIFY(testSHA224),
+                STRINGIFY(testSHA256),
+                STRINGIFY(testSHA384),
+                STRINGIFY(testSHA512),
+                STRINGIFY(testSHA512224),
+                STRINGIFY(testSHA512256)
+            };
+
+            rc = do_tests("Cryptography Test Suite", labels, testMD2,    testMD4,    testMD5,    testSHA1,      testSHA224,
+                                                             testSHA256, testSHA384, testSHA512, testSHA512224, testSHA512256,
+                                                             NULL);
+            break;
         }
-        rc = CU_get_error();
+
+        case String:
+            rc = do_hash_strings(argv[1], labels, hash_strings, digest_lengths);
+            break;
+
+        case File:
+            rc = do_hash_files(argv[2], labels, hash_files, digest_lengths);
+            break;
+
+        case Verify:
+            rc = do_verifies(argv[2], labels, hash_files, digest_lengths);
+            break;
+
+        case MAC:
+            rc = do_hmacs(argv[2], argv[3], labels, hash_hmacs, digest_lengths);
+            break;
+
+        default:
+            usage_main(*argv);
+            break;
     }
-    else if (argc == 2)
-    {
-        for (uint8_t i = 0; i < SIZE(hash_strings); ++i)
-        {
-            uint8_t *digest = /* TODO */ hash_strings[i] ? hash_strings[i](argv[1]) : NULL;
-            char *string =    /* TODO */ hash_strings[i] ? to_string(digest, digest_lengths[i]) : "(not yet implemented)";
 
-            printf("%11s: %s\n", labels[i], string);
-
-            /* TODO error handling */
-            if (hash_strings[i])
-            {
-                free(digest);
-                free(string);
-            }
-        }
-
-        rc = EXIT_SUCCESS;
-    }
-    else if (argc == 3 && STR_EQ(argv[1], "--file"))
-    {
-        for (uint8_t i = 0; i < SIZE(hash_files); ++i)
-        {
-            FILE *fp =        /* TODO */ hash_files[i] ? fopen(argv[2], "rb") : NULL;
-            uint8_t *digest = /* TODO */ hash_files[i] ? hash_files[i](fp) : NULL;
-            char *string =    /* TODO */ hash_files[i] ? to_string(digest, digest_lengths[i]) : "(not yet implemented)";
-
-            /* TODO error handling */
-            printf("%11s: %s\n", labels[i], string);
-
-            if (hash_files[i]) {
-                fclose(fp);
-                free(digest);
-                free(string);
-            }
-        }
-
-        rc = EXIT_SUCCESS;
-    }
-    else if (argc == 3 && (STR_EQ(argv[1], "--verify") || STR_EQ(argv[1], "-v")))
-    {
-        const char *filename = argv[2];
-        FILE *fp, *fhash;
-
-        /* TODO check return */
-        fp = fopen(filename, "rb");
-
-        for (uint8_t i = 0; i < SIZE(hash_files); ++i)
-        {
-            /* TODO check return */
-            fhash = open(filename, labels[i]);
-
-            if (fhash)
-            {
-                char verify[256];
-                PRINT("hashing \"%s\" with %s...\n", filename, labels[i]);
-
-                uint8_t *digest = /* TODO */ hash_files[i] ? hash_files[i](fp) : NULL;
-                char *string =    /* TODO */ hash_files[i] ? to_string(digest, digest_lengths[i]) : "(not yet implemented)";
-                PRINT("%s(%s) = %s\n", labels[i], filename, string);
-
-                fscanf(fhash, "%s", verify);
-                PRINT("found verified hash: %s\n", verify);
-
-                printf("%11s: verifying \"%s\"... ", labels[i], filename);
-                fflush(stdout);
-
-                if (STR_EQ(string, verify))
-                {
-                    printf("OK\n");
-                }
-                else
-                {
-                    printf("FAILED\n\n");
-                    printf("expected: %s\n", verify);
-                    printf("actual  : %s\n", string);
-                }
-
-                fseek(fp, 0, SEEK_SET);
-                fclose(fhash);
-
-                free(digest);
-                free(string);
-
-                memset(verify, 0, sizeof verify);
-            }
-        }
-
-        if (fp)
-        {
-            fclose(fp);
-            rc = EXIT_SUCCESS;
-        }
-    }
-    else if (argc == 4 && STR_EQ(argv[1], "--function"))
+    if (argc == 4 && STR_EQ(argv[1], "--function"))
     {
         const short index = index_of(argv[2], labels);
 
@@ -219,25 +158,6 @@ int main(int argc, char **argv)
 
             rc = EXIT_SUCCESS;
         }
-    }
-    else if (argc == 4 && (STR_EQ(argv[1], "-h") || STR_EQ(argv[1], "--hmac"))
-    {
-        const char *key = argv[2];
-        const char *msg = argv[3];
-
-        // TODO for (uint8_t i = 0; i < SIZE(hash_strings); ++i)
-        // {
-        uint8_t *digest = HMAC_MD2(key, msg);
-        char *string = to_string(digest, digest_lengths[0]);
-
-        /* TODO error handling */
-        printf("%11s: %s\n", labels[0], string);
-
-        free(digest);
-        free(string);
-
-        rc = EXIT_SUCCESS;
-        // }
     }
     else if (argc == 5 &&
                 ((STR_EQ(argv[1], "--function") && STR_EQ(argv[3], "--file")) ||
@@ -275,25 +195,12 @@ int main(int argc, char **argv)
 #undef HASHNAME
 #undef FILENAME
     }
-    else
-    {
-        printf("Usage: %s [-t] [message] [--file filename] [--function hash-function]\n\n", *argv);
-        printf("Hashes a given message or file with the specified hash function, if \n");
-        printf("one is provided. If no hash function is specified, %s will use all\n", *argv);
-        printf("available functions.\n\n");
-        printf("    -t\t\t\tRun the test suite.\n");
-        printf("    --file filename\tHash the given file with all hash functions,\n");
-        printf("                   \tunless one is provided with --function.\n");
-        printf("    --function hash\tHash the file/message with the specified hash\n");
-        printf("                   \tfunction. Can be combined with --file.\n");
-        printf("    --verify filename\tVerifies a file against the provided signatures.\n");
-    }
 
     PRINT("Exiting with status %d\n", rc);
     return rc;
 }
 
-short index_of(const char *needle, const char *haystack[])
+static short index_of(const char *needle, const char *haystack[])
 {
     for (size_t i = 0; haystack[i]; ++i)
     {
@@ -306,55 +213,189 @@ short index_of(const char *needle, const char *haystack[])
     return -1;
 }
 
-FILE *open(const char *filename, const char *ext)
+static int do_tests(const char *suite_name, const char **labels, ...)
 {
-    /* TODO need bigger int? */
-    const uint8_t sizes[] = {
-        strlen(filename),
-        strlen(ext)
-    };
-    char *hfile = malloc(sizes[0] + 1 + sizes[1] + 1);
-    PRINT("allocated %u bytes\n", sizes[0] + 1 + sizes[1] + 1);
+    bool run = true;
+    CU_pSuite suite;
+    va_list args;
 
-    FILE *fp = NULL;
-
-    if (hfile)
+    if (CU_initialize_registry() == CUE_SUCCESS)
     {
-        PRINT("copying \"%s\"...\n", filename);
-        memcpy(hfile, filename, sizes[0]);
-        PRINT("copying '%c'...\n", '.');
-        hfile[sizes[0]] = '.';
-
-        PRINT("copying \"%s\"...\n", ext);
-        memcpy(hfile + sizes[0] + 1, ext, sizes[1]);
-        PRINT("copying \\%d byte...\n", 0);
-        hfile[sizes[0] + 1 + sizes[1]] = 0;
-
-        /* remove '-', '/' */
-        PRINT("removing '%c', '%c'...\n", '-', '/');
-        char *p, *q;
-        p = q = hfile + sizes[0] + 1;
-
-        while (*p)
+        if ((suite = CU_add_suite(suite_name, NULL, NULL)))
         {
-            PRINT("*p = '%c' (0x%02x)\n", *p, *p);
-            PRINT("*q = '%c' (0x%02x)\n", *q, *q);
+            va_start(args, labels);
 
-            while (*p && (*p == '-' || *p == '/'))
+            void (*test_function)(void);
+
+            while (true)
             {
-                ++p;
+                test_function = va_arg(args, void (*)(void));
+
+                if (test_function == NULL)
+                {
+                    break;
+                }
+
+                if (!CU_add_test(suite, *labels++, test_function))
+                {
+                    printf("[warn] could not add test \"%s\"\n", *labels);
+                    break;
+                }
             }
 
-            *q++ = *p++;
+            va_end(args);
+
+            if (run)
+            {
+                CU_basic_set_mode(CU_BRM_VERBOSE);
+                CU_basic_run_tests();
+            }
         }
+        CU_cleanup_registry();
+    }
+    return run && CU_get_error();
+}
 
-        *q = 0;
+static int do_hash_strings(const char *msg, const char **labels, uint8_t *(**hash)(const char *), const uint8_t *lengths)
+{
+    for (uint8_t i = 0; i < 10; ++i)
+    {
+        uint8_t *digest = hash[i](msg);
+        char *string = to_string(digest, lengths[i]);
 
-        PRINT("opening file \"%s\"\n", hfile);
-        fp = fopen(hfile, "r");
-        PRINT("opened file \"%s\"\n", hfile);
-        free(hfile);
+        printf("%11s: %s\n", labels[i], string);
+
+        /* TODO error handling */
+        free(digest);
+        free(string);
     }
 
-    return fp;
+    return EXIT_SUCCESS;
+}
+
+static int do_hash_files(const char *filename, const char **labels, uint8_t *(**hash)(FILE *), const uint8_t *lengths)
+{
+    for (uint8_t i = 0; i < 10; ++i)
+    {
+        FILE *fp =        /* TODO */ hash[i] ? fopen(filename, "rb") : NULL;
+        uint8_t *digest = /* TODO */ hash[i] ? hash[i](fp) : NULL;
+        char *string =    /* TODO */ hash[i] ? to_string(digest, lengths[i]) : "(not yet implemented)";
+
+        /* TODO error handling */
+        printf("%11s: %s\n", labels[i], string);
+
+        if (hash[i])
+        {
+            fclose(fp);
+            free(digest);
+            free(string);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+static int do_verifies(const char *filename, const char **labels, uint8_t *(**hash)(FILE *), const uint8_t *lengths)
+{
+    FILE *fp, *fhash;
+
+    /* TODO check return */
+    fp = fopen(filename, "rb");
+
+    for (uint8_t i = 0; i < 10; ++i)
+    {
+        fhash = open(filename, labels[i]);
+
+        if (fhash)
+        {
+            char verify[256];
+            PRINT("hashing \"%s\" with %s...\n", filename, labels[i]);
+
+            uint8_t *digest = /* TODO */ hash[i] ? hash[i](fp) : NULL;
+            char *string =    /* TODO */ hash[i] ? to_string(digest, lengths[i]) : "(not yet implemented)";
+            PRINT("%s(%s) = %s\n", labels[i], filename, string);
+
+            fscanf(fhash, "%s", verify);
+            PRINT("found verified hash: %s\n", verify);
+
+            printf("%11s: verifying \"%s\"... ", labels[i], filename);
+            fflush(stdout);
+
+            if (STR_EQ(string, verify))
+            {
+                printf("OK\n");
+            }
+            else
+            {
+                printf("FAILED\n\n");
+                printf("expected: %s\n", verify);
+                printf("actual  : %s\n", string);
+            }
+
+            fseek(fp, 0, SEEK_SET);
+
+            fclose(fhash);
+            free(digest);
+            free(string);
+
+            memset(verify, 0, sizeof verify);
+        }
+    }
+
+    if (fp)
+    {
+        fclose(fp);
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
+}
+
+static int do_hmacs(const char *key, const char *msg, const char **labels, uint8_t *(**hash)(const char *, const char *), const uint8_t *lengths)
+{
+    for (uint8_t i = 0; i < 10; ++i)
+    {
+        uint8_t *digest = /* TODO */ hash[i] ? hash[i](key, msg) : NULL;
+        char *string =    /* TODO */ hash[i] ? to_string(digest, lengths[i]) : "(not yet implemented)";
+
+        /* TODO error handling */
+        printf("%11s: %s\n", labels[i], string);
+
+        if (hash[i])
+        {
+            free(digest);
+            free(string);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+static void usage_main(const char *program)
+{
+    printf("Usage: %s -t|--test\n", program);
+    printf("Usage: %s --file \"filename\"\n", program); /* TODO include --function */
+
+#if 0
+    printf("Usage: %s [--function function] \"message\"\n\n", program);
+#endif
+
+    printf("Usage: %s -h|--hmac \"key\" \"message\"\n", program); /* TODO include --function */
+    printf("Usage: %s -v|--verify \"filename\"\n\n", program);
+    printf("Hashes a given message or file with the specified hash function, if \n");
+    printf("one is provided. If no hash function is specified, %s will use all\n", program);
+    printf("available functions.\n\n");
+    printf("    -t, --test\t\t\tRun the test suite.\n");
+    printf("    --file filename\t\tHash the given file with all hash functions, unless\n");
+    printf("                   \t\tone is provided with --function.\n");
+
+#if 0
+    printf("    --function hash\tHash the file/message with the specified hash\n");
+    printf("                   \tfunction. Can be combined with --file.\n");
+#endif
+
+    printf("    --verify filename\t\tVerifies a file against the provided signature(s).\n");
+    printf("    --h, --hmac\t\t\tCalculate the HMAC hash of <message> using <key>\n");
+    printf("                   \t\tfor all hash functions, unless one is provided with\n");
+    printf("                   \t\t--function.\n");
 }
