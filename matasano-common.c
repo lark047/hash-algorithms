1c9bfc62272c2b2a65d4fa812492feae00efd99a
Andrew Clark <lark047@gmail.com>
2014-01-18 19:47:38 +0000

    added HexToCleanString and fprint_d

--- matasano-common.c
+++ matasano-common.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <ctype.h>
 
 #include "matasano-common.h"
 
@@ -14,9 +15,18 @@ void StringToHex(const char * const msg, uint8_t * const hex)
 
     for (uint64_t i = 0; i < strlen(msg) / 2; ++i)
     {
-        print_d("scanning \"%c%c\"\n", msg[2 * i], msg[2 * i + 1]);
+        // print_d("scanning \"%c%c\"\n", msg[2 * i], msg[2 * i + 1]);
         sscanf(msg + 2 * i, "%2" SCNx8, hex + i);
-        print_d("found byte 0x%02x (%u)\n", hex[i], hex[i]);
+        // print_d("found byte 0x%02x (%u)\n", hex[i], hex[i]);
+    }
+}
+
+void HexToCleanString(const uint8_t * const hex, const uint64_t length, unsigned char * const msg)
+{
+    for (uint64_t i = 0; i < length; ++i)
+    {
+        msg[i] = (isalpha(hex[i]) || hex[i] == ' ' ? hex[i] : '*');
+        // if (!(isalpha(hex[i]) || hex[i] == ' ')) { print_d("printing '*' for 0x%02x\n", hex[i]); }
     }
 }
 
@@ -37,4 +47,6 @@ static void PrintHelper(const uint8_t * const hex, const uint64_t length, const 
         printf("%02x%s", hex[i], spacer);
         fflush(stdout);
     }
+
+    printf("\n");
 }
--- matasano-common.h
+++ matasano-common.h
@@ -1,17 +1,36 @@
 #ifndef MATASANO_COMMON_H_
 #define MATASANO_COMMON_H_
 
+#define _STR(x)    #x
+#define STR(x) _STR(x)
+
 #ifdef DEBUG
-#  define print_d(f, ...)  printf("[debug] " f, __VA_ARGS__)
+#  define fprint_d(fp, f, ...) fprintf(fp, "[debug] " __FILE__ ":" STR(__LINE__) " " f, __VA_ARGS__)
+#  define print_d(f, ...)      fprint_d(stdout, f, __VA_ARGS__)
 #else
-#  define print_d(f, ...)  /* NO-OP */
+#  define fprint_d(fp, f, ...) /* NO-OP */
+#  define print_d(f, ...)      /* NO-OP */
 #endif
 
 #define STR_EQ(s1,s2) (strcmp(s1, s2) == 0)
 
 void StringToHex(const char * const, uint8_t * const);
+void HexToCleanString(const uint8_t * const, const uint64_t, unsigned char * const);
 
 void PrintHex(const uint8_t * const, const uint64_t);
 void PrintHexWithSpace(const uint8_t * const, const uint64_t);
+
+/* result of XOR decode */
+struct result
+{
+    uint8_t key;
+    double score;
+
+    /* ciphertext */
+    uint8_t *hex;
+
+    /* plaintext */
+    unsigned char *text;
+};
 
 #endif /* MATASANO_COMMON_H_ */
