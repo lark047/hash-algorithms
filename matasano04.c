4aceefe8024e7f6d28b5c290657b61ad1077af14
Andrew Clark <lark047@gmail.com>
2014-01-19 16:48:41 +0000

    removed the need for a list

--- matasano04.c
+++ matasano04.c
@@ -11,26 +11,21 @@
 /* string length (60) + newline + nul byte */
 #define BUFSIZE (60 + 1 + 1)
 
-struct node
-{
-    struct node *next;
-    struct result *result;
-};
-
-static void add_node(struct node **, struct node *);
-// static void print_list(struct node *);
-static void delete_list(struct node **);
-
 struct result *DecodeXORFromFile(FILE *fp)
 {
     char line[BUFSIZE], *p; /* TODO remove hard-coded size */
     uint8_t * const hex = malloc(30 * sizeof *hex); /* TODO ^ and check */
-    struct result *result;
-
-    struct node *head = NULL, *node;
+    struct result *result = NULL;
 
     while (fgets(line, BUFSIZE, fp) != NULL)
     {
+        int c = fgetc(fp);
+        if (!isspace(c))
+        {
+            print_d("next char is '%c' (0x%02x)\n", c, c);
+            fputc(c, fp);
+        }
+
         if ((p = strchr(line, '\n')) != NULL)
         {
             *p = 0;
@@ -46,96 +41,21 @@ struct result *DecodeXORFromFile(FILE *fp)
         StringToHex(line, hex);
         // PrintHex(hex, 30);
 
-        result = DecodeXOR(hex, 30);
+        struct result *tmp = DecodeXOR(hex, 30);
 
-        print_d("decoded with '%c' (0x%02x): %s\n", (isprint(result->key) ? result->key : ' '), result->key, result->text);
+        print_d("decoded with '%c' (0x%02x): %s\n", (isprint(tmp->key) ? tmp->key : ' '), tmp->key, tmp->text);
 
-        node = malloc(sizeof *node); /* TODO check */
-        node->next = NULL;
-        node->result = result;
-
-        add_node(&head, node);
-    }
-
-    result = head->result;
-
-    // print_list(head);
-    delete_list(&head->next);
-    free(head);
-    free(hex);
-
-    return result;
-}
-
-static void add_node(struct node **head_ref, struct node *node)
-{
-#ifdef DEBUG
-    uint8_t index = 0;
-#endif
-
-    if ((*head_ref) == NULL)
-    {
-        print_d("%s\n", "adding node at head (previously empty list)");
-        *head_ref = node;
-    }
-    else
-    {
-        struct node *current = *head_ref, *prev = NULL;
-
-        while (current && current->result->score > node->result->score)
+        if (result == NULL || result->score < tmp->score)
         {
-#ifdef DEBUG
-            ++index;
-#endif
-
-            prev = current;
-            current = current->next;
-        }
-
-        print_d("adding node at position %" PRIu8 "\n", index);
-
-        if (prev == NULL)
-        {
-            node->next = *head_ref;
-            *head_ref = node;
+            result = tmp;
         }
         else
         {
-            prev->next = node;
+            free(tmp);
         }
-
-        node->next = current;
     }
-}
 
-#if 0
-static void print_list(struct node *head)
-{
-    struct node *current = head;
-    struct result *result;
+    free(hex);
 
-    while (current)
-    {
-        result = current->result;
-        printf("Node with key '%c' and score %.5f produced \"%s\"\n", (isprint(result->key) ? result->key : ' '), result->score, result->text);
-        current = current->next;
-    }
-}
-#endif
-
-static void delete_list(struct node **head)
-{
-    struct node *current = *head;
-
-    while (current)
-    {
-        struct node *next = current->next;
-
-        free(current->result->hex);
-        free(current->result->text);
-        free(current->result);
-        free(current);
-
-        current = next;
-    }
+    return result;
 }
