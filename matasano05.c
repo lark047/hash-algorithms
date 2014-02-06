4fa8615d54197fe4dcb1e6f49a3def613f5f7d50
Andrew Clark <lark047@gmail.com>
2014-02-06 04:31:30 +0000

    removed unnecessary malloc()s

--- matasano05.c
+++ matasano05.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include "matasano.h"
 
@@ -12,17 +13,14 @@ const uint8_t *RepeatingKeyXOR(const uint8_t * const msg, uint64_t msg_length, c
         return NULL;
     }
 
-    print_d("%s\n", "");
-
-    uint8_t *buffer = malloc(msg_length + 1); /* TODO check */
-    uint8_t *hex1 = malloc(msg_length * sizeof *hex1); /* TODO check */
-    uint8_t *hex2 = malloc(msg_length * sizeof *hex2); /* TODO check */
+    print_d("start RepeatingKeyXOR(msg, %" PRIu64 ", key, %" PRIu64 ")\n", msg_length, key_length);
+    uint8_t *buffer = malloc(msg_length); /* TODO check */
 
     if (msg_length == key_length)
     {
         memcpy(buffer, key, key_length);
     }
-    else
+    else if (msg_length > key_length)
     {
         memset(buffer, 0, msg_length);
 
@@ -39,27 +37,18 @@ const uint8_t *RepeatingKeyXOR(const uint8_t * const msg, uint64_t msg_length, c
             memcpy(buffer + i, key, key_length);
         }
 
-        print_d("i = %zu\n", i);
-        // print_d("buffer is \"%s\" (%zu)\n", buffer, strlen(buffer));
-
         if (i < msg_length)
         {
-            print_d("%zu > %zu\n", i, msg_length);
+            print_d("%zu < %zu\n", i, msg_length);
             memcpy(buffer + i, key, msg_length - i);
         }
 
-        print_d("i = %zu\n", i);
-        // print_d("buffer is \"%s\" (%zu)\n", buffer, strlen(buffer));
+        print_d("i = %zu, buffer is:\n", i);
+        PrintHex(buffer, msg_length, true);
     }
 
-    memcpy(hex1, msg, msg_length);
-    memcpy(hex2, buffer, msg_length);
-
-    const uint8_t *result = FixedXOR(hex1, hex2, msg_length);
-
+    const uint8_t *result = FixedXOR(msg, buffer, msg_length);
     free(buffer);
-    free(hex1);
-    free(hex2);
 
     return result;
 }
